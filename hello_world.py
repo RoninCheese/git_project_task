4835f853e390c2e1bb644d5dcb372c35e2be9216
RoninCheese <yarnoldmiles@gmail.com>
2025-02-01 18:08:57 +0200

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,17 @@
-print("Hello world")
+
+
+def get_user_name():
+    while True:
+        user_name = input("Please enter your name: ")
+        if user_name.isalpha(): 
+            return user_name
+        else:
+            print("Invalid input. Please enter only letters.")
+
+def greet_user(name):
+    print(f"Hello, {name}!")
+
+
+if __name__ == "__main__":
+    name = get_user_name()  
+    greet_user(name)
